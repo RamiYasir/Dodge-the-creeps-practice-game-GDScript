b5b22c96cbb59aabb52537f4d103e30dab5f17ab
RamiYasir <81882740+RamiYasir@users.noreply.github.com>
2021-05-12 15:31:19 +0100

    Update Player.gd

--- DodgeTheCreeps/Player.gd
+++ DodgeTheCreeps/Player.gd
@@ -49,47 +49,6 @@ func start(pos):
 	show()
 	$CollisionShapeBody.disabled = false
 	$CollisionShapeLegs.disabled = false
-	
-
-## my attempt at trying to make the thing accelerate.
-#
-#const var MAX_SPEED = 600
-#var speed = 400
-##export var speed = 400
-##export var damping_speed = 0.0
-#var screen_size 
-#
-#func _ready():
-#	screen_size = get_viewport_rect().size 
-#
-#func _process(delta):	
-#	var initial_velocity = Vector2()
-#	var final_velocity = initial_velocity
-#	var acceleration = Vector2()
-#
-#	# do some stuff before normalizing the velocity and multiplying it by speed in order to get acceleration up to max speed
-#
-#	if Input.is_action_pressed("ui_right"):
-#		initial_velocity.x += 1
-#	if Input.is_action_pressed("ui_left"):
-#		initial_velocity.x -= 1
-#	if Input.is_action_pressed("ui_down"):
-#		initial_velocity.y += 1
-#	if Input.is_action_pressed("ui_up"):
-#		initial_velocity.y -= 1
-#	if initial_velocity.length() > 0:
-#		final_velocity = initial_velocity.normalized() * speed
-#		acceleration = (final_velocity - initial_velocity) / delta
-#		initial_velocity = final_velocity
-#		$AnimatedSprite.play()
-#	else:
-#		$AnimatedSprite.stop()
-#
-##	position += final_velocity * acceleration
-#	position += final_velocity * acceleration
-#	position.x = clamp(position.x, 0, screen_size.x)
-#	position.y = clamp(position.y, 0, screen_size.y)
-
 
 
 
